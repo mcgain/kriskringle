7cb1a9b42d08413478c8c485c30d6bfaf2ac708c
Richard McGain <mcgain@gmail.com>
2012-12-31 04:42:05 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kriskringle::Application.config.secret_token = 'f71fea0549bc79bc83b3f969730c3d87d776f2d9e20914f83203b105b0ea3807102bb9262cc0eeed7d3100dabd09aef179601aaa3d722754d373f31abbd56daf'
